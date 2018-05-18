1abf47ae039ed4b9f6040e128d4bfc2706c2257b
oshmoun <oshmoun100@googlemail.com>
2018-05-18 13:17:13 +0200

    BoardConfig: Add a default TARGET_BOOTLOADER_BOARD_NAME
    There is no need to break the build when a TARGET_BOOTLOADER_BOARD_NAME can not be derived from TARGET_PRODUCT.
    Simply set a valid default value, and warn the user about the issue.
    This is useful to guarantee compatibility with projects based on Sony's Open Devices that change TARGET_PRODUCT to fit their needs.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,8 @@ TARGET_BOOTLOADER_BOARD_NAME := H8314
 else ifneq (,$(filter %h8324,$(TARGET_PRODUCT)))
 TARGET_BOOTLOADER_BOARD_NAME := H8324
 else
-$(error Unrecognized value for TARGET_PRODUCT: "$(TARGET_PRODUCT)")
+TARGET_BOOTLOADER_BOARD_NAME := H8314
+$(warning Unrecognized value for TARGET_PRODUCT: "$(TARGET_PRODUCT)", using default value: "$(TARGET_BOOTLOADER_BOARD_NAME)")
 endif
 
 # Platform
